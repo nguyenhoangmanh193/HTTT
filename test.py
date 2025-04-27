fdeb8b300237318b4b0f22b62d254d8b34d1af9a
nguyenhoangmanh193 <onmontoan@gmail.com>
2025-04-27 21:57:42 +0700

    ff

--- test.py
+++ test.py
@@ -19,13 +19,3 @@ df = df.reset_index(drop=True)
 df['clean_comment'] = df['clean_comment'].fillna('').astype(str)
 df['Label'] = df['Label'].fillna('').astype(int)
 df['Labels'] = df['clean_comment'].apply(lambda x: model.classify_sentiment(x)[0])
-
-# Tạo một cột mới để đánh dấu sự khác nhau giữa hai cột
-df['Label_diff'] = df['Label'] != df['Labels']
-
-# Hiển thị các hàng mà hai cột khác nhau
-different_rows = df[df['Label_diff'] == True]
-
-print(f"Số lượng hàng khác nhau: {len(different_rows)}")
-print(different_rows[['Label', 'Labels']])
-df.to_excel('ten_file.xlsx', index=False, engine='openpyxl')
